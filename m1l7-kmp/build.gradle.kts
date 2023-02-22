e8a2534ab4cb3fc339fd9bcd49ae0e5add1e3930
Зяблов Антон <ahton070@gmail.com>
2023-02-22 19:46:32 +0300

    Добавлены учебные модули

---
+++ m1l7-kmp/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    kotlin("multiplatform")
+}
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+        withJava()
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+
+    js(BOTH) {
+        browser {}
+    }
+
+    val coroutinesVersion: String by project
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion")
+            }
+        }
+        val jvmMain by getting
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val jsMain by getting {
+            dependencies {
+                implementation(npm("js-big-decimal", "~1.3.4"))
+                implementation(npm("is-sorted", "~1.0.5"))
+            }
+        }
+        val jsTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,21 @@
-rootProject.name = "ZyablovMarketplace"
+rootProject.name = "ZyablovAdsCar"
 
 pluginManagement {
     val kotlinVersion: String by settings
+    val kotestVersion: String by settings
 
     plugins {
         kotlin("jvm") version kotlinVersion
+        kotlin("multiplatform") version kotlinVersion apply false
+        id("io.kotest.multiplatform") version kotestVersion apply false
     }
 }
 
 include("m1l1-quickstart")
+include("m1l1-hello-world")
+include("m1l3-oop")
+include("m1l4-dsl")
+include("m1l5-coroutines")
+include("m1l6-flows-and-channels")
+include("m1l7-kmp")
+include("m2l2-testing")
